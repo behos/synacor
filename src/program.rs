8217e5466747551811fb5fb79117d2d3156cef38
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2021-05-16 12:19:38 +0300

    Dump program to reverse engineer it when the confirmation runs

--- src/program.rs
+++ src/program.rs
@@ -1,8 +1,10 @@
 use crate::{
     memory::{Memory, MAX_ADDR, MAX_INT},
     operations::Operation,
-    values::Value,
+    values::{Register, Value},
 };
+
+use anyhow::{bail, Result};
 use std::io;
 
 struct InputBuffer {
@@ -49,31 +51,66 @@ impl Program {
     }
 
     fn step(&mut self) -> bool {
-        let opcode = Operation::from(self.memory.at(self.cursor));
-        match opcode {
-            Operation::Out => self.run_out(),
-            Operation::Input => self.run_input(),
-            Operation::Equal => self.run_equal(),
-            Operation::GreaterThan => self.run_greater_than(),
-            Operation::And => self.run_and(),
-            Operation::Or => self.run_or(),
-            Operation::Not => self.run_not(),
-            Operation::Call => self.run_call(),
-            Operation::Return => self.run_return(),
-            Operation::ReadMem => self.run_read_mem(),
-            Operation::WriteMem => self.run_write_mem(),
-            Operation::Set => self.run_set(),
-            Operation::Push => self.run_push(),
-            Operation::Pop => self.run_pop(),
-            Operation::Jump => self.run_jump(),
-            Operation::JumpTrue => self.run_jump_true(),
-            Operation::JumpFalse => self.run_jump_false(),
-            Operation::Add => self.run_add(),
-            Operation::Modulo => self.run_modulo(),
-            Operation::Multiply => self.run_multiply(),
+        let operation = self.next_operation().expect("should be a valid operation");
+        match operation {
             Operation::Halt => self.run_halt(),
+            Operation::Set(r, v) => self.run_set(r, v),
+            Operation::Push(v) => self.run_push(v),
+            Operation::Pop(r) => self.run_pop(r),
+            Operation::Equal(r, v1, v2) => self.run_equal(r, v1, v2),
+            Operation::GreaterThan(r, v1, v2) => self.run_greater_than(r, v1, v2),
+            Operation::Jump(v) => self.run_jump(v),
+            Operation::JumpTrue(v1, v2) => self.run_jump_true(v1, v2),
+            Operation::JumpFalse(v1, v2) => self.run_jump_false(v1, v2),
+            Operation::Add(r, v1, v2) => self.run_add(r, v1, v2),
+            Operation::Multiply(r, v1, v2) => self.run_multiply(r, v1, v2),
+            Operation::Modulo(r, v1, v2) => self.run_modulo(r, v1, v2),
+            Operation::And(r, v1, v2) => self.run_and(r, v1, v2),
+            Operation::Or(r, v1, v2) => self.run_or(r, v1, v2),
+            Operation::Not(r, v) => self.run_not(r, v),
+            Operation::ReadMem(r, v) => self.run_read_mem(r, v),
+            Operation::WriteMem(v1, v2) => self.run_write_mem(v1, v2),
+            Operation::Call(v) => self.run_call(v),
+            Operation::Return => self.run_return(),
+            Operation::Out(v) => self.run_out(v),
+            Operation::Input(r) => self.run_input(r),
             Operation::Noop => self.run_noop(),
         }
+    }
+
+    fn next_operation(&mut self) -> Result<Operation> {
+        let pos = self.cursor;
+        let opcode = self.memory.at(self.cursor);
+        let operation = match opcode {
+            0 => Operation::Halt,
+            1 => Operation::Set(self.next_register()?, self.next_value()),
+            2 => Operation::Push(self.next_value()),
+            3 => Operation::Pop(self.next_register()?),
+            4 => Operation::Equal(self.next_register()?, self.next_value(), self.next_value()),
+            5 => {
+                Operation::GreaterThan(self.next_register()?, self.next_value(), self.next_value())
+            }
+            6 => Operation::Jump(self.next_value()),
+            7 => Operation::JumpTrue(self.next_value(), self.next_value()),
+            8 => Operation::JumpFalse(self.next_value(), self.next_value()),
+            9 => Operation::Add(self.next_register()?, self.next_value(), self.next_value()),
+            10 => Operation::Multiply(self.next_register()?, self.next_value(), self.next_value()),
+            11 => Operation::Modulo(self.next_register()?, self.next_value(), self.next_value()),
+            12 => Operation::And(self.next_register()?, self.next_value(), self.next_value()),
+            13 => Operation::Or(self.next_register()?, self.next_value(), self.next_value()),
+            14 => Operation::Not(self.next_register()?, self.next_value()),
+            15 => Operation::ReadMem(self.next_register()?, self.next_value()),
+            16 => Operation::WriteMem(self.next_value(), self.next_value()),
+            17 => Operation::Call(self.next_value()),
+            18 => Operation::Return,
+            19 => Operation::Out(self.next_value()),
+            20 => Operation::Input(self.next_register()?),
+            21 => Operation::Noop,
+            a if a > 21 => bail!("invalid opcode {}", a),
+            _ => unimplemented!("Missing implementation for opcode {}", opcode),
+        };
+        log::debug!("[{:05}] {:?}", pos, operation);
+        Ok(operation)
     }
 
     pub fn execute(&mut self) {
@@ -82,18 +119,16 @@ impl Program {
     }
 
     fn run_noop(&mut self) -> bool {
-        log::debug!("Running noop");
         self.cursor += 1;
         false
     }
 
     fn run_halt(&mut self) -> bool {
-        log::debug!("Running halt");
         true
     }
 
-    fn run_out(&mut self) -> bool {
-        let arg = self.next_value();
+    fn run_out(&mut self, arg: Value) -> bool {
+        let arg = self.val(arg);
         if arg > 127 {
             panic!("Invalid character byte {:?}", arg)
         } else {
@@ -103,24 +138,22 @@ impl Program {
         false
     }
 
-    fn run_input(&mut self) -> bool {
-        let reg = self.next_register();
+    fn run_input(&mut self, reg: Register) -> bool {
         let mut value = self.input_buffer.next();
 
         if value as char == '!' {
-            log::debug!("bypassing register");
+            log::debug!("overriding register");
             self.memory.set_register(7, 666);
             value = self.input_buffer.next()
         }
 
-        self.memory.set_register(reg, value as u16);
+        self.memory.set_register(reg.into(), value as u16);
         self.cursor += 1;
         false
     }
 
-    fn run_jump(&mut self) -> bool {
-        let arg = self.next_value();
-        log::debug!("jump: {}", arg);
+    fn run_jump(&mut self, arg: Value) -> bool {
+        let arg = self.val(arg);
         if (arg as usize) < MAX_ADDR {
             self.cursor = arg as usize
         } else {
@@ -129,10 +162,10 @@ impl Program {
         false
     }
 
-    fn run_jump_true(&mut self) -> bool {
-        let cond = self.next_value();
-        let arg = self.next_value();
-        log::debug!("jump true: {} {}", cond, arg);
+    fn run_jump_true(&mut self, cond: Value, arg: Value) -> bool {
+        let cond = self.val(cond);
+        let arg = self.val(arg);
+
         if cond == 0 {
             self.cursor += 1
         } else {
@@ -141,10 +174,9 @@ impl Program {
         false
     }
 
-    fn run_jump_false(&mut self) -> bool {
-        let cond = self.next_value();
-        let arg = self.next_value();
-        log::debug!("jump true: {} {}", cond, arg);
+    fn run_jump_false(&mut self, cond: Value, arg: Value) -> bool {
+        let cond = self.val(cond);
+        let arg = self.val(arg);
         if cond != 0 {
             self.cursor += 1
         } else {
@@ -153,102 +185,100 @@ impl Program {
         false
     }
 
-    fn run_set(&mut self) -> bool {
-        let reg = self.next_register();
-        let val = self.next_value();
-        log::debug!("set: {} {}", reg, val);
-        self.memory.set_register(reg, val);
+    fn run_set(&mut self, reg: Register, val: Value) -> bool {
+        let val = self.val(val);
+        self.memory.set_register(reg.into(), val);
         self.cursor += 1;
         false
     }
 
-    fn run_push(&mut self) -> bool {
-        let val = self.next_value();
-        log::debug!("push: {}", val);
+    fn run_push(&mut self, val: Value) -> bool {
+        let val = self.val(val);
         self.memory.push(val);
         self.cursor += 1;
         false
     }
 
-    fn run_pop(&mut self) -> bool {
-        let reg = self.next_register();
+    fn run_pop(&mut self, reg: Register) -> bool {
         let val = self.memory.pop().expect("Should have a value when popping");
-        log::debug!("pop: {}", val);
-        self.memory.set_register(reg, val);
+        self.memory.set_register(reg.into(), val);
         self.cursor += 1;
         false
     }
 
-    fn run_add(&mut self) -> bool {
-        log::debug!("add");
-        self.run_calc(|a, b| a + b)
+    fn run_add(&mut self, reg: Register, v1: Value, v2: Value) -> bool {
+        self.run_calc(reg, v1, v2, |a, b| a + b)
     }
 
-    fn run_modulo(&mut self) -> bool {
-        log::debug!("modulo");
-        self.run_calc(|a, b| a % b)
+    fn run_modulo(&mut self, reg: Register, v1: Value, v2: Value) -> bool {
+        self.run_calc(reg, v1, v2, |a, b| a % b)
     }
 
-    fn run_multiply(&mut self) -> bool {
-        log::debug!("multiply");
-        self.run_calc(|a, b| ((a as usize * b as usize) % MAX_INT as usize) as u16)
+    fn run_multiply(&mut self, reg: Register, v1: Value, v2: Value) -> bool {
+        self.run_calc(reg, v1, v2, |a, b| {
+            ((a as usize * b as usize) % MAX_INT as usize) as u16
+        })
     }
 
-    fn run_and(&mut self) -> bool {
-        log::debug!("and");
-        self.run_calc(|a, b| a & b)
+    fn run_and(&mut self, reg: Register, v1: Value, v2: Value) -> bool {
+        self.run_calc(reg, v1, v2, |a, b| a & b)
     }
 
-    fn run_or(&mut self) -> bool {
-        log::debug!("or");
-        self.run_calc(|a, b| a | b)
+    fn run_or(&mut self, reg: Register, v1: Value, v2: Value) -> bool {
+        self.run_calc(reg, v1, v2, |a, b| a | b)
     }
 
-    fn run_calc(&mut self, calc: impl FnOnce(u16, u16) -> u16) -> bool {
-        let reg = self.next_register();
-        let a = self.next_value();
-        let b = self.next_value();
+    fn run_calc(
+        &mut self,
+        reg: Register,
+        a: Value,
+        b: Value,
+        calc: impl FnOnce(u16, u16) -> u16,
+    ) -> bool {
+        let a = self.val(a);
+        let b = self.val(b);
         let res = calc(a, b) % MAX_INT;
-        log::debug!("set {} {}", reg, res);
-        self.memory.set_register(reg, res);
+        self.memory.set_register(reg.into(), res);
         self.cursor += 1;
         false
     }
 
-    fn run_not(&mut self) -> bool {
-        let reg = self.next_register();
-        let val = self.next_value();
-        log::debug!("not: {} {}", reg, val);
-        self.memory.set_register(reg, !val % MAX_INT);
+    fn run_not(&mut self, reg: Register, val: Value) -> bool {
+        let val = self.val(val);
+        self.memory.set_register(reg.into(), !val % MAX_INT);
         self.cursor += 1;
         false
     }
 
-    fn run_equal(&mut self) -> bool {
-        log::debug!("equal");
-        self.run_cond(|a, b| a == b)
+    fn run_equal(&mut self, reg: Register, v1: Value, v2: Value) -> bool {
+        self.run_cond(reg, v1, v2, |a, b| a == b)
     }
 
-    fn run_greater_than(&mut self) -> bool {
-        log::debug!("greater than");
-        self.run_cond(|a, b| a > b)
+    fn run_greater_than(&mut self, reg: Register, v1: Value, v2: Value) -> bool {
+        self.run_cond(reg, v1, v2, |a, b| a > b)
     }
 
-    fn run_cond(&mut self, comp: impl FnOnce(u16, u16) -> bool) -> bool {
-        let reg = self.next_register();
-        let a = self.next_value();
-        let b = self.next_value();
-        log::debug!("comp {} {}", a, b);
+    fn run_cond(
+        &mut self,
+        reg: Register,
+        a: Value,
+        b: Value,
+        comp: impl FnOnce(u16, u16) -> bool,
+    ) -> bool {
+        let a = self.val(a);
+        let b = self.val(b);
         let val = if comp(a, b) { 1 } else { 0 };
-        log::debug!("set {} {}", reg, val);
-        self.memory.set_register(reg, val);
+        self.memory.set_register(reg.into(), val);
         self.cursor += 1;
         false
     }
 
-    fn run_call(&mut self) -> bool {
-        let val = self.next_value();
-        log::debug!("call: {}", val);
+    fn run_call(&mut self, val: Value) -> bool {
+        let val = self.val(val);
+        // 6027 is the test function. Let's dump the program when it's called
+        if val == 6027 {
+            self.dump()
+        }
         self.memory.push((self.cursor + 1) as u16);
         self.cursor = val as usize;
         false
@@ -257,47 +287,57 @@ impl Program {
     fn run_return(&mut self) -> bool {
         if let Some(addr) = self.memory.pop() {
             self.cursor = addr as usize;
-            log::debug!("return: {}", addr);
             false
         } else {
-            log::debug!("return root");
             true
         }
     }
 
-    fn run_read_mem(&mut self) -> bool {
-        let reg = self.next_register();
-        let addr = self.next_value();
+    fn run_read_mem(&mut self, reg: Register, addr: Value) -> bool {
+        let addr = self.val(addr);
         let val = self.memory.at(addr as usize);
-        log::debug!("set: {} {}", reg, val);
-        self.memory.set_register(reg, val);
+        self.memory.set_register(reg.into(), val);
         self.cursor += 1;
         false
     }
 
-    fn run_write_mem(&mut self) -> bool {
-        let addr = self.next_value();
-        let val = self.next_value();
-        log::debug!("set: {} {}", addr, val);
-        self.memory.set(addr as usize, val);
+    fn run_write_mem(&mut self, addr: Value, val: Value) -> bool {
+        self.memory.set(self.val(addr) as usize, self.val(val));
         self.cursor += 1;
         false
     }
 
-    /// Retrives the next value from memory and steps the cursor
-    fn next_value(&mut self) -> u16 {
+    fn next_value(&mut self) -> Value {
         self.cursor += 1;
-        match Value::from(self.memory.at(self.cursor)) {
-            Value::Register(r) => self.memory.read_register(r as usize),
+        Value::from(self.memory.at(self.cursor))
+    }
+
+    // Resolves the value either from a reference or from a literal
+    fn val(&self, val: Value) -> u16 {
+        match val {
+            Value::Register(Register(r)) => self.memory.read_register(r as usize),
             Value::Literal(num) => num,
         }
     }
 
-    fn next_register(&mut self) -> usize {
+    fn next_register(&mut self) -> Result<Register> {
         self.cursor += 1;
-        match Value::from(self.memory.at(self.cursor)) {
-            Value::Register(r) => r as usize,
-            Value::Literal(num) => panic!("expected register but got literal {}", num),
+        let val = Value::from(self.memory.at(self.cursor));
+        match val {
+            Value::Register(r) => Ok(r),
+            Value::Literal(num) => bail!("expected register but got literal {}", num),
         }
+    }
+
+    fn dump(&mut self) {
+        let initial_cursor = self.cursor;
+        self.cursor = 0;
+        log::debug!("Dumping program.");
+        while self.cursor < MAX_ADDR {
+            self.next_operation().ok();
+            self.cursor += 1;
+        }
+        log::debug!("Dumping done.");
+        self.cursor = initial_cursor;
     }
 }
--- src/values.rs
+++ src/values.rs
@@ -1,14 +1,24 @@
 #[derive(Debug)]
+pub struct Register(pub u8);
+
+impl From<Register> for usize {
+    fn from(reg: Register) -> usize {
+        let Register(val) = reg;
+        val as usize
+    }
+}
+
+#[derive(Debug)]
 pub enum Value {
     Literal(u16),
-    Register(u8),
+    Register(Register),
 }
 
 impl From<u16> for Value {
     fn from(raw: u16) -> Self {
         match raw {
             r if r <= 32767 => Value::Literal(raw),
-            r if r <= 32775 => Value::Register((r - 32768) as u8),
+            r if r <= 32775 => Value::Register(Register((r - 32768) as u8)),
             _ => panic!("Invalid value {}", raw),
         }
     }
@@ -18,7 +28,7 @@ impl From<Value> for u16 {
     fn from(val: Value) -> u16 {
         match val {
             Value::Literal(v) => v,
-            Value::Register(v) => v as u16,
+            Value::Register(Register(v)) => v as u16,
         }
     }
 }
